edaa42cc8647452be2757a4afe0eb247b4230799
Kakhi <kakhaberi.kiknadze.1@gmail.com>
2022-09-24 16:32:37 +0400

    Added unit tests

--- Tests/SwiftUICoordinatorTests/SwiftUICoordinatorTests.swift
+++ Tests/SwiftUICoordinatorTests/SwiftUICoordinatorTests.swift
@@ -1,5 +1,161 @@
 import XCTest
+import SwiftUI
+import Combine
 @testable import SwiftUICoordinator
 
 final class SwiftUICoordinatorTests: XCTestCase {
+    private var cancellables: Set<AnyCancellable>!
+    
+    override func setUp() {
+        super.setUp()
+        cancellables = []
+    }
+    
+    // MARK: - Presentation style
+    
+    func test_coordination_sheet_presentation() {
+        let coordinator: SwiftUICoordinator<Void> = .init(id: "1", mode: .normal)
+        let presentedCoordinator: SwiftUICoordinator<Void> = .init(id: "2", mode: .normal)
+
+        _ = coordinator.coordinate(to: presentedCoordinator, presentationStyle: .sheet)
+        XCTAssertEqual(presentedCoordinator.presentationStyle, .sheet)
+    }
+    
+    func test_coordination_fullScreen_presentation() {
+        let coordinator: SwiftUICoordinator<Void> = .init(id: "1", mode: .normal)
+        let presentedCoordinator: SwiftUICoordinator<Void> = .init(id: "2", mode: .normal)
+
+        _ = coordinator.coordinate(to: presentedCoordinator, presentationStyle: .fullScreen)
+        XCTAssertEqual(presentedCoordinator.presentationStyle, .fullScreen)
+    }
+    
+    func test_coordination_custom_presentation() {
+        let coordinator: SwiftUICoordinator<Void> = .init(id: "1", mode: .normal)
+        let presentedCoordinator: SwiftUICoordinator<Void> = .init(id: "2", mode: .normal)
+
+        _ = coordinator.coordinate(to: presentedCoordinator, presentationStyle: .custom(.slide))
+        XCTAssertEqual(presentedCoordinator.presentationStyle, .custom(.scale))
+    }
+    
+    func test_coordination_sheet_finish() {
+        let coordinator: SwiftUICoordinator<Void> = .init(id: "1", mode: .normal)
+        let presentedCoordinator: SwiftUICoordinator<String> = .init(id: "2", mode: .normal)
+        let expectedResult = "Result"
+
+        let expectation = self.expectation(description: "coordination finish")
+        coordinator.coordinate(to: presentedCoordinator, presentationStyle: .sheet)
+            .sink(receiveCompletion: { completion in
+                expectation.fulfill()
+            }, receiveValue: { result in
+                XCTAssertEqual(result, expectedResult)
+            })
+            .store(in: &cancellables)
+        
+        presentedCoordinator.finish(result: expectedResult)
+        waitForExpectations(timeout: 0.1)
+    }
+    
+    // MARK: - Finish
+    
+    func test_coordination_fullScreen_finish() {
+        let coordinator: SwiftUICoordinator<Void> = .init(id: "1", mode: .normal)
+        let presentedCoordinator: SwiftUICoordinator<String> = .init(id: "2", mode: .normal)
+        let expectedResult = "Result"
+
+        let expectation = self.expectation(description: "coordination finish")
+        coordinator.coordinate(to: presentedCoordinator, presentationStyle: .fullScreen)
+            .sink(receiveCompletion: { completion in
+                expectation.fulfill()
+            }, receiveValue: { result in
+                XCTAssertEqual(result, expectedResult)
+            })
+            .store(in: &cancellables)
+        
+        presentedCoordinator.finish(result: expectedResult)
+        waitForExpectations(timeout: 0.1)
+    }
+    
+    func test_coordination_custom_finish() {
+        let coordinator: SwiftUICoordinator<Void> = .init(id: "1", mode: .normal)
+        let presentedCoordinator: SwiftUICoordinator<String> = .init(id: "2", mode: .normal)
+        let expectedResult = "Result"
+
+        let expectation = self.expectation(description: "coordination finish")
+        coordinator.coordinate(to: presentedCoordinator, presentationStyle: .custom(.scale))
+            .sink(receiveCompletion: { completion in
+                expectation.fulfill()
+            }, receiveValue: { result in
+                XCTAssertEqual(result, expectedResult)
+            })
+            .store(in: &cancellables)
+        
+        presentedCoordinator.finish(result: expectedResult)
+        waitForExpectations(timeout: 0.1)
+    }
+    
+    func test_coordination_navigation_finish() {
+        let coordinator: SwiftUICoordinator<Void> = .init(id: "1", mode: .navigation)
+        let presentedCoordinator: SwiftUICoordinator<String> = .init(id: "2", mode: .normal)
+        let expectedResult = "Result"
+        let scene = coordinator.getRoot()
+        
+        let expectation = self.expectation(description: "coordination finish")
+        XCTAssertNotNil(coordinator.navigationRouter)
+        coordinator.navigationRouter!.push(presentedCoordinator)
+            .sink(receiveCompletion: { completion in
+                expectation.fulfill()
+            }, receiveValue: { result in
+                
+            })
+            .store(in: &cancellables)
+
+        presentedCoordinator.finish(result: expectedResult)
+        waitForExpectations(timeout: 0.1)
+    }
+    
+    func test_coordinator_finish() {
+        let presentedCoordinator: SwiftUICoordinator<String> = .init(id: "1", mode: .normal)
+        let expectation = self.expectation(description: "coordinator finish")
+        let expectedResult = "Result"
+        
+        presentedCoordinator.onFinish
+            .sink { completion in
+                expectation.fulfill()
+            } receiveValue: { result in
+                XCTAssertEqual(result, expectedResult)
+            }
+            .store(in: &cancellables)
+
+        presentedCoordinator.finish(result: expectedResult)
+        waitForExpectations(timeout: 0.1)
+    }
+    
+    // MARK: - Cancel
+    
+    func test_coordinator_cancel() {
+        let presentedCoordinator: SwiftUICoordinator<String> = .init(id: "1", mode: .normal)
+        let expectation = self.expectation(description: "coordinator cancel")
+        
+        presentedCoordinator.onCancel
+            .sink { completion in
+                expectation.fulfill()
+            } receiveValue: { _ in }
+            .store(in: &cancellables)
+
+        presentedCoordinator.cancel()
+        waitForExpectations(timeout: 0.1)
+    }
+}
+
+extension ModalPresentationStyle: Equatable {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        switch (lhs, rhs) {
+        case (.sheet, .sheet),
+            (.fullScreen, .fullScreen),
+            (.custom, .custom):
+            return true
+        default:
+            return false
+        }
+    }
 }
