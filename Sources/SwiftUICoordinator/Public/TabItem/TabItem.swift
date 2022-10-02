08aa6b51cdc1d57749f5a5e7e37a2b01e11394d2
Kakhi <kakhaberi.kiknadze.1@gmail.com>
2022-10-02 17:01:26 +0400

    Added Documentation comments on TabItem

--- Sources/SwiftUICoordinator/Public/TabItem/TabItem.swift
+++ Sources/SwiftUICoordinator/Public/TabItem/TabItem.swift
@@ -5,9 +5,16 @@
 //  Created by Kakhaberi Kiknadze on 28.09.22.
 //
 
+/// Tab item to be used inside tab view.
 public struct TabItem: Hashable {
+    /// Tab title.
     let title: String
+    
+    /// Tab icon. Can be either local or remote.
+    /// Local one also supports system icons.
     let image: ImageSource
+    
+    /// Badge count to be shown on a tab item.
     let badgeCount: Int
     
     public init(
@@ -22,6 +29,7 @@ public struct TabItem: Hashable {
 }
 
 public extension TabItem {
+    /// Creates a `TabItem` with no title, image and badge.
     static var empty: Self {
         .init(title: "", image: .local(.normal("")))
     }
